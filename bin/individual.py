bc3424fca7d3ec848bd0c848f30e0d146fb7a918
Robert <robertgcampbell95@gmail.com>
2019-09-19 15:14:39 -0700

    Remove psnr callback

--- bin/individual.py
+++ bin/individual.py
@@ -1,32 +1,6 @@
 from genetics import Genotype
 from tensorflow.keras.callbacks import Callback
 
-class PSNRCallback(Callback):
-    def __init__(self, X, Y, batch_size):
-        #super().__init__()
-        self.X_train = X
-        self.Y_train = Y
-        self.batch_size = batch_size
-
-
-    def on_train_begin(self, logs={}):
-        self.test_psnr = []
-        self.ssim = []
-
-    def on_train_end(self, logs=None):
-        from skimage.measure import compare_psnr
-        from skimage.measure import compare_ssim
-        import numpy as np 
-
-        train_prediction = np.asarray(self.model.predict(self.X_train, batch_size=self.batch_size, verbose=0))
-        psnr = compare_psnr(np.asarray(self.Y_train), train_prediction.astype(self.Y_train.dtype))
-        #ssim = compare_ssim(self.X_train, train_prediction, multichannel=True)
-
-        self.test_psnr.append(psnr)
-
-        print("- test psnr: {}".format(psnr))
-        #print(" - ssim: {}".format(ssim))
-
 class Individual():
     def __init__(self, config, new_genotype=False):
         self.batch_size = int(config["ml"]["batch_size"])
@@ -90,9 +64,7 @@ class Individual():
         # Model callbacks
         early_stopping = EarlyStopping(monitor='loss', mode='min', verbose=1, patience=self.epochs // 10)
         checkpoint = ModelCheckpoint('./model.h5', monitor='loss', mode='min', save_best_only=True, verbose=1)
-
-        # psnr = PSNRCallback(x_test, y_test, self.batch_size)
-
+        
         # Remove psnr from callbacks, for now
         history = self.model.fit(dataset, epochs=self.epochs, batch_size = None, steps_per_epoch=50, shuffle=True, callbacks=[checkpoint, early_stopping])
         # history.history["test_psnr"] = psnr.test_psnr
