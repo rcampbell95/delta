8a848f3b2bf718afb88643689cdc297a84470e1a
Robert <robertgcampbell95@gmail.com>
2019-11-22 17:47:47 -0800

    Remove skip connections from symmetric autoencoder search space

--- bin/conv_autoencoder.py
+++ bin/conv_autoencoder.py
@@ -4,7 +4,6 @@ from genetics import Gene, Genotype
 def define_gene(self):
     self.params = {"filter_size": (16, 32, 64, 128),
                     "kernel_size": (1, 3, 5, 7),
-                    "skip": (True, False),
                     "regularization": ("spatial_dropout", "dropout"),
                     "dropout_rate": (0.0, 0.1, 0.3, 0.5),
                     "activation": ("selu", "relu", "tanh"),
@@ -49,7 +48,6 @@ class ConvAutoencoderGenotype(Genotype):
             x = Conv2D(filters = gene.attrs["filter_size"], kernel_size = kernel_size, 
                         padding = "same")(x)
 
-            x = MaxPooling2D(pool_size=(pool_size, pool_size))(x)
             x = Activation(gene.attrs["activation"])(x)
 
             if gene.attrs["regularization"] == "spatial_dropout":
@@ -57,26 +55,15 @@ class ConvAutoencoderGenotype(Genotype):
             elif gene.attrs["regularization"] == "dropout":
                 x = Dropout(rate=gene.attrs["dropout_rate"])(x)
 
-            if gene.attrs["skip"]:
-                skip_values.append(x)
-
-
         # Build decoder
         if shape == "symmetric":
             for idx, gene in enumerate(coding_sequence):
                 kernel_size = (gene.attrs["kernel_size"], gene.attrs["kernel_size"])
 
-                
-                x = UpSampling2D(size=(pool_size, pool_size))(x)
                 x = Conv2DTranspose(filters=gene.attrs["filter_size"], 
                                     kernel_size=kernel_size, 
                                     padding="same")(x)
                 
-                if gene.attrs["skip"]:
-                    try:
-                        x = Add()([skip_values.pop(), x])
-                    except:
-                        print(gene.node_id)
                 x = Activation(gene.attrs["activation"])(x)
 
                 if gene.attrs["regularization"] == "spatial_dropout":
@@ -89,14 +76,9 @@ class ConvAutoencoderGenotype(Genotype):
         # Add decoder layer for asymmetric autoencoder or add output layer for symmetric autoencoder
         if shape == "symmetric" or coding_sequence[0].attrs["output"] == "transpose":
             # Define output deconvolutional layer
-            if shape == "symmetric":
-                strides = (1, 1)
-            else:
-                strides = (2 ** num_nodes, 2 ** num_nodes)
 
             output = Conv2DTranspose(filters=out_channels, 
-                                     kernel_size=out_kernel_size, 
-                                     strides=strides, 
+                                     kernel_size=1, 
                                      name="output")(x)
         # Add decoder for flatten + dense asymmetric autoencoder
         elif coding_sequence[0].attrs["output"] == "dense" and shape == "asymmetric":
@@ -117,7 +99,7 @@ class ConvAutoencoderGenotype(Genotype):
             except:
                 print("Training using CPU or single GPU")
 
-            model.compile(optimizer="adam",
+        model.compile(optimizer="adam",
                     loss='mse')
             
         return model
