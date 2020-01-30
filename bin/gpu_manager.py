64fb7e356f797db95526ae5ba3baf0eeab66c7ba
Robert <robertgcampbell95@gmail.com>
2020-01-29 16:39:50 -0800

    Manage list of available devices in gpu manager

--- bin/gpu_manager.py
+++ bin/gpu_manager.py
@@ -1,43 +1,25 @@
 import multiprocessing
-import queue
-from tensorflow.python.client import device_lib
+import tensorflow as tf
 
 
 class GPU_Manager():
     def __init__(self):
-        self.gpu_queue = queue.Queue()
-        self.default_device = "/device:CPU:0"
-        local_device_protos = device_lib.list_local_devices()
+        self.device_queue = multiprocessing.Queue()
 
-        gpu_count = 0
-        for x in local_device_protos: 
-            if "gpu" in x.device_type.lower():
-                gpu_count += 1
-                self.gpu_queue.put(x.name) 
+        self.cpus = [x.name for x in tf.config.experimental.list_logical_devices('CPU')]
+        self.gpus = [x.name for x in tf.config.experimental.list_logical_devices('GPU')]
+
+        gpu_count = len(self.gpus)
+
         if gpu_count == 0:
-            self.gpu_available = False
-            
+            for cpu in self.cpus:
+                self.device_queue.put(cpu)
+        elif gpu_count > 0:
+            for gpu in self.gpus:
+                self.device_queue.put(gpu)
+
     def request_device(self):
-        if self.gpu_available:
-            gpu_name = self.gpu_queue.get()
-            return gpu_name
-        else:
-            return self.default_device
+        return self.device_queue.get()
 
     def free_device(self, device_name):
-        self.gpu_queue.put(device_name)
-
-
-manager = GPU_Manager()
-
-device_name = manager.request_device()
-
-print(device_name)
-
-manager.free_device(device_name)
-
-print(manager.request_device())
-
-print(manager.request_device())
-
-manager.request_device()
+        self.device_queue.put(device_name)
