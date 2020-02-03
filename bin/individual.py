7c19ccbe6d2880929679b17eb4fc2a60a512629e
Robert <robertgcampbell95@gmail.com>
2020-02-03 14:33:13 -0800

    Use delta.ml.train for autoencoder training

--- bin/individual.py
+++ bin/individual.py
@@ -3,54 +3,36 @@ from tensorflow.keras.callbacks import Callback
 import tensorflow as tf
 import os 
 import multiprocessing
+import sys
 
-def load_data(config_values):
-    from delta.imagery import imagery_dataset
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from delta.config import config
+from delta.imagery import imagery_dataset
+from delta.ml.train import train
 
-    batch_size = config_values['ml']['batch_size']
-    num_epochs = config_values["ml"]["num_epochs"]
+def assemble_dataset():
 
-    print('loading data from ' + config_values['input_dataset']['data_directory'])
-    aeds_train = imagery_dataset.AutoencoderDataset(config_values)
-    train_ds = aeds_train.dataset(filter_zero=False)
+    # Use wrapper class to create a Tensorflow Dataset object.
+    # - The dataset will provide image chunks and corresponding labels.
+    tc = config.training()
 
-    train_ds = train_ds.repeat(num_epochs).batch(batch_size)
+    ids = imagery_dataset.AutoencoderDataset(config.images(), config.labels(), config.chunk_size(), tc.chunk_stride)
 
-    train_directory = config_values['input_dataset']['data_directory']
-
-    print('loading validation data from ' + config_values['input_dataset']['val_directory'])
-    config_values['input_dataset']['data_directory'] = config_values['input_dataset']['val_directory']
-    aeds_val = imagery_dataset.AutoencoderDataset(config_values)
-    val_ds = aeds_val.dataset(filter_zero=False)
-
-    val_ds = val_ds.repeat(num_epochs).batch(batch_size)
-
-    config_values['input_dataset']['data_directory'] = train_directory
- 
-    return train_ds, val_ds
+    return ids
 
 class Individual(multiprocessing.Process):
     fitness_queue = multiprocessing.Queue(0)
 
-
-    def __init__(self, config, new_genotype=False, child_index=0):
+    def __init__(self, config_values, new_genotype=False, child_index=0):
         multiprocessing.Process.__init__(self)
-
-
 
         self.history = None
         self.child_index = child_index
-        self.batch_size = int(config["ml"]["batch_size"])
-        self.epochs = int(config["ml"]["num_epochs"])
-        self.steps_per_epoch = int(config["ml"]["steps_per_epoch"])
-        self.config = config
-        self.model_path = os.path.join(self.config["ml"]["model_folder"], self.config["ml"]["model_dest_name"])
-
-        self.output_folder = os.path.join(self.config["ml"]["output_folder"], str(child_index))
+        self.config_values = config_values
 
         if new_genotype == False:
-            self.genotype = ConvAutoencoderGenotype(config)
+            self.genotype = ConvAutoencoderGenotype(config_values)
         else:
             self.genotype = new_genotype
 
@@ -58,95 +40,51 @@ class Individual(multiprocessing.Process):
         self.genotype.mutate_hidden_genes()
 
     def generate_child(self, child_index):
-        child_genotype = self.genotype.replicate(self.config)
-        child = Individual(self.config, child_genotype, child_index)
+        child_genotype = self.genotype.replicate(self.config_values)
+        child = Individual(self.config_values, child_genotype, child_index)
         return child
 
     @classmethod
     def histories(cls):
         histories = []
         while cls.fitness_queue.qsize() > 0:
-            print(cls.fitness_queue.qsize())
             msg = cls.fitness_queue.get(block=False)
             histories.append(msg[1])
             
         return histories
 
 
-    def evaluate_fitness(self, trainset, valset):
-        from tensorflow.keras.utils import plot_model
-        from tensorflow.keras.callbacks import ModelCheckpoint
-        from tensorflow.keras.callbacks import EarlyStopping
-        import tensorflow as tf
-        from contextlib import redirect_stdout
-        import pandas as pd
-        import numpy as np
-        
-        chunk_size = int(self.config["ml"]["chunk_size"])
-        channels = int(self.config["ml"]["channels"])
+    def build_model(self):        
+        chunk_size = config.chunk_size()
+        channels = int(self.config_values["ml"]["channels"])
 
         input_shape = (chunk_size, chunk_size, channels)
 
-        model = self.genotype.build_model(self.config, input_shape)
+        model = self.genotype.build_model(self.config_values, input_shape)
+
+        return model
 
         # Save model summary to text file
         # TODO -- make child directories if not available
         # TODO -- best option would be to make temp directory and delete after training
-        # TODO -- not high priority though
-        with open(os.path.join(self.config["ml"]["output_folder"], str(self.child_index), 'modelsummary.txt'), 'w') as f:
-            with redirect_stdout(f):
-                model.summary()
 
         # Save genetics to csv
-        gene_attrs = {}
-        for gene in self.genotype.genes:
-            for attr, val in gene.attrs.items():
-                if attr in gene_attrs:
-                    gene_attrs[attr].append(val)
-                else:
-                    gene_attrs[attr] = [val]
-            if "Connection id" in gene_attrs:
-                gene_attrs["Connection id"].append(gene.conn)
-            else:
-                gene_attrs["Connection id"] = [gene.conn]
+        #gene_attrs = {}
+        #for gene in self.genotype.genes:
+        #    for attr, val in gene.attrs.items():
+        #        if attr in gene_attrs:
+        #            gene_attrs[attr].append(val)
+        #        else:
+        #            gene_attrs[attr] = [val]
+        #    if "Connection id" in gene_attrs:
+        #        gene_attrs["Connection id"].append(gene.conn)
+        #    else:
+        #        gene_attrs["Connection id"] = [gene.conn]
 
-        pd.DataFrame(gene_attrs).to_csv(os.path.join(self.config["ml"]["output_folder"], str(self.child_index), "genotype.csv"))
-
-        # Model callbacks
-        early_stopping = EarlyStopping(monitor=self.config["evolutionary_search"]["metric"], 
-                                       mode='min', 
-                                       verbose=1, 
-                                       patience=self.epochs // 10)
-
-        filepath = os.path.join(self.config["ml"]["model_folder"], str(self.child_index), self.config["ml"]["model_dest_name"])
-        checkpoint = ModelCheckpoint(filepath       = filepath, 
-                                     monitor        = self.config["evolutionary_search"]["metric"], 
-                                     mode           = 'min', 
-                                     save_best_only = True, 
-                                     verbose        = 1)
-        
-        # Remove psnr from callbacks, for now
-        history = model.fit(x                = trainset, 
-                            epochs           = self.epochs, 
-                            batch_size       = None, 
-                            steps_per_epoch  = self.steps_per_epoch, 
-                            shuffle          = True, 
-                            callbacks        = [checkpoint, early_stopping],
-                            validation_data  = valset,
-                            validation_steps = None if valset is None else self.steps_per_epoch)
-        #self.history = history
-        # history.history["test_psnr"] = psnr.test_psnr
-        return history
-        # start thread
-        # load config values
-        # get gpu 
-        # set up context
-        # load data
-        # train model
-        # return fitness value
-
+        #pd.DataFrame(gene_attrs).to_csv(os.path.join(self.config["ml"]["output_folder"], str(self.child_index), "genotype.csv"))
 
     def run(self):
+        # TODO Set up context with GPU
         from gpu_manager import GPU_Manager
 
         device_manager = GPU_Manager()
@@ -154,13 +92,11 @@ class Individual(multiprocessing.Process):
 
         with tf.Graph().as_default() as g:
             with tf.device(device):
-                trainset, valset = load_data(self.config)
+                train_spec = config.training()
+                ids = assemble_dataset()
 
-                history = self.evaluate_fitness(trainset, valset)
+                model, history = train(self.build_model, ids, train_spec)
 
                 msg = (self.child_index, history.history)
 
                 self.fitness_queue.put(msg)
-                
-                print(id(self.fitness_queue))
-                print(self.fitness_queue.qsize())
