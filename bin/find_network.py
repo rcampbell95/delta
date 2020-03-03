d3d27a3032de75525ca4f877931f8facbd759433
Robert <robertgcampbell95@gmail.com>
2020-03-03 14:33:29 -0800

    Log artifacts to temp folder

--- bin/find_network.py
+++ bin/find_network.py
@@ -1,37 +1,59 @@
+import os
+import tempfile
+import shutil
+
+import mlflow
+import numpy as np
+
 from gpu_manager import GPU_Manager
 from individual import Individual
 
-# def log_artifacts(config_values, fittest_index):
-#     #mlflow.log_artifact("./model.png")
-#     output_folder = os.path.join(config.output_folder(), str(fittest_index))
-#     model_name = config_values["ml"]["model_dest_name"]
+def log_artifacts(config_values, fittest_index):
+    output_folder = os.path.join(config_values["ml"]["output_folder"], str(fittest_index))
+    model_name = "model.h5"
 
-#     assert(os.path.exists(os.path.join(output_folder, model_name)))
-#     assert(os.path.exists(os.path.join(output_folder, "modelsummary.txt")))
-#     assert(os.path.exists(os.path.join(output_folder, "genotype.csv")))
+    assert os.path.exists(os.path.join(output_folder, model_name))
+    assert os.path.exists(os.path.join(output_folder, "genotype.csv"))
 
-#     mlflow.log_artifact(os.path.join(output_folder, model_name))
-#     mlflow.log_artifact(os.path.join(output_folder, "modelsummary.txt"))
-#     mlflow.log_artifact(os.path.join(output_folder, "genotype.csv"))
+    mlflow.log_artifact(os.path.join(output_folder, model_name))
+    mlflow.log_artifact(os.path.join(output_folder, "genotype.csv"))
 
+def create_tmp_dirs(num_children):
+    sysTemp = tempfile.gettempdir()
+
+    temproot = tempfile.mkdtemp(dir=sysTemp)
+
+    for child in range(num_children):
+        dirname = os.path.join(temproot, str(child))
+
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+
+    return temproot
+
+def cleanup_tmp_dirs(tmp_dir):
+    shutil.rmtree(tmp_dir)
 
 def find_network(config_values):
-    #log = True if config_values["evolutionary_search"]["log"].lower() == "true" else False
+    log = config_values["evolutionary_search"]["log"].lower() == "true"
 
-    #if log:
-    #    #mlflow.set_tracking_uri("file:./mlruns")
-    #    mlflow.start_run()
+    if log:
+        output_folder = create_tmp_dirs(config_values["evolutionary_search"]["num_children"])
+        print(output_folder)
+        config_values["ml"]["output_folder"] = output_folder
+        mlflow.set_tracking_uri("file:./mlruns")
+        mlflow.set_experiment(config_values["ml"]["experiment_name"])
+        mlflow.start_run()
 
-    #    for category in config_values.keys():
-    #        for param in config_values[category].keys():
-    #            mlflow.log_param(param, config_values[category][param])
+        for category in config_values.keys():
+            for param in config_values[category].keys():
+                mlflow.log_param(param, config_values[category][param])
 
-
+    device_manager = GPU_Manager()
     ###
     # Train and evaluate generation 0
     ###
     print("Generation 0")
-    device_manager = GPU_Manager()
     parent = Individual(config_values, device_manager)
 
     parent.start()
@@ -40,11 +62,13 @@ def find_network(config_values):
     best_history = Individual.histories()[0]
     metric_best = min(best_history[config_values["evolutionary_search"]["metric"]])
 
-    #if log:
-    #    log_artifacts(config_values, 0)
+    if log:
+        log_artifacts(config_values, 0)
     #    # Log parent as baseline for best model
-    #    for metric in best_history.keys():
-    #        mlflow.log_metric(metric, min(best_history[metric]), step=0)
+
+        epoch_index = np.argmin(best_history[config_values["evolutionary_search"]["metric"]])
+        for metric, value in best_history.items():
+            mlflow.log_metric(metric, value[epoch_index], step=0)
 
     for generation in range(1, int(config_values["evolutionary_search"]["generations"]) + 1):
         print("\n\n\nGeneration ", generation)
@@ -69,25 +93,32 @@ def find_network(config_values):
             print("Child {} is more fit than parent.".format(idx_fittest + 1))
             print("Parent for next generation will be child {}".format(idx_fittest + 1))
 
-            #if log:
-            #    best_history = child_histories[idx_fittest]
-            #    for metric in child_histories[idx_fittest].keys():
-            #        mlflow.log_metric(metric, min(child_histories[idx_fittest][metric]), step=generation)
-            #        print("Logged {} to mlflow".format(metric))
-            #    try:
-            #        log_artifacts(config_values, idx_fittest)
-            #        print("Logged architecture")
-            #    except:
-            #        print("Artifact can't be logged!")
+            if log:
+                best_history = child_histories[idx_fittest]
+                epoch_index = np.argmin(best_history[config_values["evolutionary_search"]["metric"]])
+
+                for metric, value in best_history.items():
+                    mlflow.log_metric(metric, value[epoch_index], step=generation)
+                    print("Logged {} to mlflow".format(metric))
+                try:
+                    log_artifacts(config_values, idx_fittest)
+                    print("Logged architecture")
+                except AssertionError:
+                    print("Artifact can't be logged!")
         else:
-            #for metric in best_history.keys():
-            #    mlflow.log_metric(metric, min(best_history[metric]), step=generation)
+            if log:
+                epoch_index = np.argmin(best_history[config_values["evolutionary_search"]["metric"]])
+                for metric, value in best_history.items():
+
+                    mlflow.log_metric(metric, value[epoch_index], step=generation)
             print("Children were less fit than parent model. Continuing with parent for next generation")
             parent.self_mutate()
 
 
-    #if log:
-    #    mlflow.end_run()
+    if log:
+        mlflow.end_run()
+        cleanup_tmp_dirs(output_folder)
+
 
 if __name__ == "__main__":
     ###
@@ -96,17 +127,18 @@ if __name__ == "__main__":
     evolution_config = {
         "evolutionary_search" : {
             "grid_height": 10,
-            "grid_width": 1,
-            "level_back": 1,
-            "num_children": 2,
-            "generations": 20,
-            "r": .1,
+            "grid_width": 3,
+            "level_back": 2,
+            "num_children": 1,
+            "generations": 100,
+            "r": .3,
             "metric": "val_loss",
-            "log": True,
-            "shape": "asymmetric"
+            "log": "true",
+            "shape": "symmetric",
         },
         "ml": {
-            "channels": 7
+            "channels": 7,
+            "experiment_name": "test_r_val"
         }
     }
 
